a4963a395b9cce8ecacda138bdfa7bdcb9183833
Roshan-shetty20 <roshanshetty0431@gmail.com>
2025-03-26 08:28:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+    fetch("/transactions")
+        .then(response => response.json())
+        .then(data => {
+            console.log("Transactions:", data);
+        });
+});
